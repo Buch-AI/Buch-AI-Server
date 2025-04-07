418535ca9b6247750d773656e2735f7dc6370b17
Julian Wong <julianwong2000@gmail.com>
2025-04-07 22:29:28 +0100

    Updated required Terraform version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "google" {
   credentials = file(var.gcp_svc_key_file)
   project     = var.gcp_project_id
