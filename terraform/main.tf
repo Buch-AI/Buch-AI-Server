48556d25f1b1d7043d457e17ce05f68796d5dc3d
Julian Wong <julianwong2000@gmail.com>
2025-04-07 23:57:00 +0100

    Added revsion suffixes to ends of Cloud Run objects to force re-deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ resource "google_cloud_run_service" "server" {
   location = var.gcp_region
 
   template {
+    metadata {
+      annotations = {
+        "run.googleapis.com/revision-suffix" = substr(md5(timestamp()), 0, 4)
+      }
+    }
+
     spec {
       containers {
         image = var.server_image_tag
@@ -47,10 +53,8 @@ resource "google_cloud_run_v2_job" "video_generator" {
     }
   }
 
-  lifecycle {
-    ignore_changes = [
-      template[0].template[0].containers[0].image
-    ]
+  annotations = {
+    "run.googleapis.com/revision-suffix" = substr(md5(timestamp()), 0, 4)
   }
 }
 
