67248533f184d8f82d312084dceec81a120491ab
Julian Wong <julianwong2000@gmail.com>
2025-04-13 19:52:36 +0100

    Updated environment variable names to avoid clashes with existing ones

--- config.py
+++ config.py
@@ -6,9 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Environment configuration
-ENV = os.getenv("ENV", "p").lower()
+ENV = os.getenv("BUCHAI_ENV", "p").lower()
 if ENV not in ["d", "p"]:
-    raise ValueError("ENV must be either 'd' (development) or 'p' (production)")
+    raise ValueError("BUCHAI_ENV must be either 'd' (development) or 'p' (production)")
+
+# API Keys
+AUTH_JWT_KEY = os.getenv("BUCHAI_AUTH_JWT_KEY")
+if not AUTH_JWT_KEY:
+    raise ValueError("BUCHAI_AUTH_JWT_KEY environment variable is not set")
+
+HF_API_KEY = os.getenv("BUCHAI_HF_API_KEY")
+if not HF_API_KEY:
+    raise ValueError("BUCHAI_HF_API_KEY environment variable is not set")
+
+# Google Cloud Storage URIs
+GCLOUD_STB_CREATIONS_NAME = "bai-buchai-p-stb-usea1-creations"
 
 # Project root directory
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -17,15 +29,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 ASSETS_DIR = os.path.join(PROJECT_ROOT, "app", "assets")
 ASSETS_D_DIR = os.path.join(ASSETS_DIR, "d")
 ASSETS_P_DIR = os.path.join(ASSETS_DIR, "p")
-
-# Google Cloud Storage URIs
-GCLOUD_STB_CREATIONS_NAME = "bai-buchai-p-stb-usea1-creations"
-
-# API Keys
-AUTH_JWT_KEY = os.getenv("AUTH_JWT_KEY")
-if not AUTH_JWT_KEY:
-    raise ValueError("AUTH_JWT_KEY environment variable is not set")
-
-HF_API_KEY = os.getenv("HF_API_KEY")
-if not HF_API_KEY:
-    raise ValueError("HF_API_KEY environment variable is not set")
